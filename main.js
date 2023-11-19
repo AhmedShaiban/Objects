3108040d1423191d9c13f974cfb4396452026e56
AhmedShaiban <143512500+AhmedShaiban@users.noreply.github.com>
2023-11-19 22:51:33 +0100

    Objects

---
+++ main.js
@@ -0,0 +1,290 @@
+//Opdracht 1
+const persoon = {
+  voornaam: "Ahmed",
+  achternaam: "Shaiban",
+  leeftijd: 26,
+  opleiding: "ICT",
+  score: 98,
+};
+
+console.log(persoon);
+
+ //Opdracht 2
+ const post = {
+  gebruikersnaam: "Bard",
+  titel: "De mooiste plek in Nederland",
+  verhaal: "Ik heb vandaag een prachtige wandeling gemaakt door het Nationaal Park De Hoge Veluwe. Het is echt een aanrader om hier eens te gaan wandelen. De natuur is er prachtig en er is veel te zien.",
+  datum: "2023-11-16",
+  views: 1000,
+};
+
+console.log(post);
+
+//Opdracht 3
+const recepten = [
+  {
+    titel: "Voedzame salade",
+    beschrijving: "Deze salade is boordevol vitamines en mineralen. Hij is perfect voor een lichte lunch of diner.",
+    gezond: true,
+  },
+  {
+    titel: "Gezonde pizza",
+    beschrijving: "Deze pizza is gemaakt met volkorenmeel en veel groenten. Hij is een gezondere versie van de traditionele pizza.",
+    gezond: true,
+  },
+  {
+    titel: "Lentegroentenoep",
+    beschrijving: "Deze soep is gemaakt met verse lentegroenten. Hij is licht en smaakvol en perfect voor een warme dag.",
+    gezond: true,
+  },
+];
+
+console.log(recepten);
+
+ //Opdracht 4
+ let Ford_Fiesta = {title: 'Ford Fiesta 1.0 EcoBoost 100 PK. TITANIUM, LUXE UITVOERING',
+ description: 'Aantal deuren: 5. Kleur: Zwart metallic. Technische informatie. Vermogen: 74 kW (100 PK). Aantal cilinders: 3. Transmissie: 6 versnellingen, Handgeschakeld. Aandrijving: Voorwielaandrijving. Bandenmaat: 195/55 R16. Acceleratie (0-100): 10,6 s. Topsnelheid: 183 km/u',
+ price: 9.999,
+ kilometer: 178.548,
+ buildDate: 2019};
+
+ console.log(Ford_Fiesta);
+ 
+
+ let KIA_Niro ={title: 'KIA Niro 1.6 GDi Hybrid 141pk Dct6 2017 Blauw',
+ description: 'Hybride. Bouwjaar 2017. Kenteken 12-7-2017. APK tot 7-2025. Kilometerstand ca. 82.000. Schadevrije auto. Kleur Deep Ceruelean Blue. Altijd dealer onderhouden (Content Autogroep) en valt dus nog binnen de 7 jaar fabrieksgarantie. Heeft vorige maand kleine beurt gehad. Lichtmetalen velgen. Navigatie. Automaat. Lederen bekleding. AC.',
+ price: 17.900,
+ kilometer: 82.000,
+ buildDate: 2017};
+
+ console.log(KIA_Niro);
+ 
+
+ let Ford_Custom = {title: '29x Ford Custom 310 2.0 TDCI L1L2 Airco Cruise Cam Navi LED',
+ description: 'Ford Transit Custom 270/280/290/300 101/105/109/131/170 pk 2.0/2.2 TDCI L1/L2 6-Bak Enkel/Dubbel cabine 2800KG Trekgewicht Airco Cruisecontrol Achteruitrij camera Imperiaal Parkeersensoren Kasten Navigatie Lane-assist 16 Inch Lichtmetalen velgen 3/5/6 Persoons Elektrischpakket 2xSchuifdeur Start/Stop-systeem Sidebars Stoelverwarming Trekhaak Metallic lak Apple CarPlay LED-verlichting.',
+ price: 5.750,
+ kilometer: 154.597,
+ buildDate: 2018};
+
+ console.log(Ford_Custom);
+
+
+ let Volkswagen_GOLF = {title: 'Volkswagen GOLF Variant 1.4 TSI Highline Automaat Bj 2011 De',
+ description: '12Volt aansluiting, Achterspoiler, Achteruitrijcamera, Airco, Airco (automatisch), Airco met elektronische regeling, Alarm klasse 3, Armsteun achter, Audio-navigatie full map + hard disk, Audio installatie',
+ price: 5.250,
+ kilometer: 272.096,
+ buildDate: 2011};
+
+ console.log(Volkswagen_GOLF);
+ 
+
+ let Volkswagen_GOLF_TSI = {title: 'Volkswagen Golf, 2.0 TSI GTI Clubsport HUD/Memory/Pano/Blindspot',
+ description: 'Bomvolle GTI Clubsport dat is voorzien van o.a Panoramadak / Dynaudio / Memory stoel / Lederen bekleding / Blindspot / Camera / Head-Up display etc. Prijs is inclusief BTW!',
+ price: 42.950,
+ kilometer:31.009 ,
+ buildDate: 2021};
+
+ console.log(Volkswagen_GOLF_TSI);
+ 
+  //Opdracht 5
+  let Mosselen  = {title: '1 de Beste Mosselen medium',
+  beschrijving: 'Mosselen (Mytilus edulis)',
+  actie: '2 kg',
+  prijs: 6.49};
+
+  console.log(Mosselen);
+
+ 
+
+  let Corte_Fresca = {title: 'Corte Fresca Pinot grigio',
+  beschrijving: 'Geldig van woensdag 15 november t/m dinsdag 21 november 2023',
+  actie: '750 ml',
+  prijs: 3.99};
+
+  console.log(Corte_Fresca);
+  
+  let Uien = {title: 'Bio+ Uien',
+  beschrijving: 'Geldig van woensdag 15 november t/m dinsdag 21 november 2023',
+  actie: '500 g',
+  prijs: 0.59};
+
+
+  console.log(Uien);
+
+
+  let mandarijnen = {title: '1 de Beste Grote mandarijnen verpakt',
+  beschrijving: 'Geldig van woensdag 15 november t/m dinsdag 21 november 2023',
+  actie: '1,5 kg',
+  prijs: 2.49 };
+
+  console.log(mandarijnen);
+  
+
+  let Winterpeen = {title: 'Hollandse Winterpeen',
+  beschrijving: 'Geldig van woensdag 15 november t/m dinsdag 21 november 2023',
+  actie: '1 kg (ca. 4 stuks)',
+  prijs: 0.78};
+
+  console.log(Winterpeen);
+
+
+//Opdracht 5
+
+
+
+//Opdracht 7
+
+const recepten = [
+  {
+    titel: "Voedzame salade",
+    beschrijving: "Deze salade is boordevol vitamines en mineralen. Hij is perfect voor een lichte lunch of diner.",
+    gezond: true,
+  },
+  {
+    titel: "Gezonde pizza",
+    beschrijving: "Deze pizza is gemaakt met volkorenmeel en veel groenten. Hij is een gezondere versie van de traditionele pizza.",
+    gezond: true,
+  },
+  {
+    titel: "Lentegroentenoep",
+    beschrijving: "Deze soep is gemaakt met verse lentegroenten. Hij is licht en smaakvol en perfect voor een warme dag.",
+    gezond: true,
+  },
+];
+for (const recept of recepten) {
+  console.log(recept);
+}
+
+
+//Opdracht 8
+const aanbiedingen1 = [
+  {
+    title: "1 de Beste Mosselen medium",
+    beschrijving: "Mosselen (Mytilus edulis)",
+    actie: "2 kg",
+    prijs: 6.49,
+  },
+  {
+    title: "Corte Fresca Pinot grigio",
+    beschrijving: "Geldig van woensdag 15 november t/m dinsdag 21 november 2023",
+    actie: "750 ml",
+    prijs: 3.99,
+  },
+  {
+    title: "Bio+ Uien",
+    beschrijving: "Geldig van woensdag 15 november t/m dinsdag 21 november 2023",
+    actie: "500 g",
+    prijs: 0.59,
+  },
+  {
+    title: "1 de Beste Grote mandarijnen verpakt",
+    beschrijving: "Geldig van woensdag 15 november t/m dinsdag 21 november 2023",
+    actie: "1,5 kg",
+    prijs: 2.49,
+  },
+  {
+    title: "Hollandse Winterpeen",
+    beschrijving: "Geldig van woensdag 15 november t/m dinsdag 21 november 2023",
+    actie: "1 kg (ca. 4 stuks)",
+    prijs: 0.78,
+  },
+];
+for (const aanbieding of aanbiedingen) {
+  console.log(aanbieding);
+}
+
+//Opdracht 9
+
+const aanbiedingen2 = [
+  {
+    title: "1 de Beste Mosselen medium",
+    beschrijving: "Mosselen (Mytilus edulis)",
+    actie: "2 kg",
+    prijs: 6.49,
+  },
+  {
+    title: "Corte Fresca Pinot grigio",
+    beschrijving: "Geldig van woensdag 15 november t/m dinsdag 21 november 2023",
+    actie: "750 ml",
+    prijs: 3.99,
+  },
+  {
+    title: "Bio+ Uien",
+    beschrijving: "Geldig van woensdag 15 november t/m dinsdag 21 november 2023",
+    actie: "500 g",
+    prijs: 0.59,
+  },
+  {
+    title: "1 de Beste Grote mandarijnen verpakt",
+    beschrijving: "Geldig van woensdag 15 november t/m dinsdag 21 november 2023",
+    actie: "1,5 kg",
+    prijs: 2.49,
+  },
+  {
+    title: "Hollandse Winterpeen",
+    beschrijving: "Geldig van woensdag 15 november t/m dinsdag 21 november 2023",
+    actie: "1 kg (ca. 4 stuks)",
+    prijs: 0.78,
+  },
+];
+
+const container1 = document.querySelector(".aanbiedingen-container");
+
+for (const aanbieding of aanbiedingen) {
+  const card = document.createElement("div");
+  card.classList.add("aanbieding");
+
+  const titel = document.createElement("h3");
+  titel.innerText = aanbieding.title;
+  card.appendChild(titel);
+
+  const beschrijving = document.createElement("p");
+  beschrijving.innerText = aanbieding.beschrijving;
+  card.appendChild(beschrijving);
+
+  const actie = document.createElement("p");
+  actie.innerText = aanbieding.actie + " voor €" + aanbieding.prijs;
+  card.appendChild(actie);
+
+  container.appendChild(card);
+}
+
+//Opdracht 10
+const recepten2 = [
+  {
+    title: "Voedzame salade",
+    beschrijving: "Deze salade is boordevol vitamines en mineralen. Hij is perfect voor een lichte lunch of diner.",
+    gezond: true,
+  },
+  {
+    title: "Gezonde pizza",
+    beschrijving: "Deze pizza is gemaakt met volkorenmeel en veel groenten. Hij is een gezondere versie van de traditionele pizza.",
+    gezond: true,
+  },
+  {
+    title: "Lentegroentenoep",
+    beschrijving: "Deze soep is gemaakt met verse lentegroenten. Hij is licht en smaakvol en perfect voor een warme dag.",
+    gezond: true,
+  },
+];
+
+const container = document.querySelector(".recepten-container");
+
+for (const recept of recepten) {
+  const card = document.createElement("div");
+  card.classList.add("recept");
+
+  const titel = document.createElement("h3");
+  titel.innerText = recept.title;
+  card.appendChild(titel);
+
+  const beschrijving = document.createElement("p");
+  beschrijving.innerText = recept.beschrijving;
+  card.appendChild(beschrijving);
+
+  const gezond = document.createElement("p");
+  gezond.innerText = "Gezond: " + recept.gezond;
+  card.appendChild(gezond);
+
+  container.appendChild(card);
+}
